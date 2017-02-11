20a4aa96cfab824dc43d712813827e0ba3d47a7c
Figo Han <spectacularfigo@gmail.com>
2017-02-10 21:12:29 -0500

    Verion 1.1

---
+++ SwiftWB/SwiftWB/Main/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  SwiftWB
+//
+//  Created by Figo Han on 2017-02-09.
+//  Copyright © 2017 Figo Han. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftWB/SwiftWB/Main/ToolClass/FHasdf.swift
@@ -0,0 +1,13 @@
+//
+//  FHasdf.swift
+//  SwiftWB
+//
+//  Created by Figo Han on 2017-02-09.
+//  Copyright © 2017 Figo Han. All rights reserved.
+//
+
+import UIKit
+
+class FHasdf: NSObject {
+
+}
