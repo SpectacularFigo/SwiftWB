89e803aa87b11dd7650d23513a093a470fcb1ee1
Figo Han <>
2018-04-01 19:35:05 -0400

    Coding Style Changes on BaseTableView

--- SwiftWB/SwiftWB/Main/FHBaseTableViewController.swift
+++ SwiftWB/SwiftWB/Main/FHBaseTableViewController.swift
@@ -9,73 +9,50 @@
 import UIKit
 
 class FHBaseTableViewController: UITableViewController {
-    
-    // MARK:- LazyLoading
-    var isLogin : Bool
-        {
-            
-     var savePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-    
-     savePath = (savePath as NSString).appendingPathComponent("account.plist")
-        
+    fileprivate var isLogin : Bool {
+        var savePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        savePath = (savePath as NSString).appendingPathComponent("account.plist")
         let account = NSKeyedUnarchiver.unarchiveObject(withFile: savePath) as? FHUserAccount
-        
         if account==nil {
             return false
         } else {
             return true
         }
-    
     }
+    
     fileprivate lazy var vistorView = FHVisitorView.visitorView()
     
-    
-    
-    
-    
-    // MARK:- Others
+    // MARK:- Life Cycyle
     override func loadView() {
-        
         isLogin ? super.loadView() : setupVisitorView()
-        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-    
 }
 
 // MARK:- UI Structure Setting
-extension FHBaseTableViewController
-{
-
+extension FHBaseTableViewController {
     func setupVisitorView()  {
-        self.view=vistorView
+        self.view = vistorView
         navigationItem.leftBarButtonItem=UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerNavigationItemClick))
         navigationItem.rightBarButtonItem=UIBarButtonItem(title: "Login", style: .plain, target: self, action: #selector(loginNavigationBarItemCick))
     }
-    
 }
 
 
 // MARK:- Events
-extension FHBaseTableViewController
-{
-    
+extension FHBaseTableViewController {
     @objc fileprivate func registerNavigationItemClick(){
-        
         print("Register")
     }
-    
     
     @objc fileprivate func loginNavigationBarItemCick(){
         let navigationController = UINavigationController()
         navigationController.addChildViewController(FHOAuthViewController())
         self.present(navigationController, animated: true, completion: nil)
     }
-    
-    
 }
 
 
